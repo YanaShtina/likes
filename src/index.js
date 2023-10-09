d7f36feddf08f897ad682d4f3de580284af62763
Iana Shtina <sianalucky@gmail.com>
2023-10-09 23:18:13 +0300

    hero

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import './index.scss';
 import scroll  from './modules/scrollTo';
 import popup  from './modules/popup';
-import mask  from './modules/mask';
-
-
+import burger  from './modules/burger';
 
 
 scroll.scroll();
 popup.init();
-mask.init();
+burger.init();
 
 
 
---
+++ src/modules/burger.js
@@ -0,0 +1,26 @@
+const bodyScrollLock = require('body-scroll-lock');
+const disableBodyScroll = bodyScrollLock.disableBodyScroll;
+const enableBodyScroll = bodyScrollLock.enableBodyScroll;
+
+export default {
+  init() {
+    const burger = document.querySelector('.burger');
+    const body = document.querySelector('body');
+    const nav = document.querySelector('.nav__wrap');
+    
+
+    burger.addEventListener('click', () => {
+      if (burger.classList.contains('active')) {
+        burger.classList.remove('active');
+        body.classList.remove('active');
+        enableBodyScroll(nav);
+      } else {
+        burger.classList.add('active');
+        body.classList.add('active'); 
+   
+        disableBodyScroll(nav);
+      }
+     nav.classList.toggle('active');
+    })
+  },
+}
